98c9c081791df5d7d7723e79ef560500b7f9f6be
johowjo <johowjo@gmail.com>
2024-06-16 20:42:31 +0800

    changed CompileRun(), deleted comments in some files

--- lua/plugins_config/telescope.lua
+++ lua/plugins_config/telescope.lua
@@ -1,43 +1,6 @@
--- return {
--- 	{
--- 		"nvim-telescope/telescope.nvim",
--- 		tag = "0.1.5",
--- 		dependencies = { "nvim-lua/plenary.nvim" },
--- 		config = function()
--- 			local builtin = require("telescope.builtin")
--- 			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
--- 			vim.keymap.set("n", "<C-o>", builtin.live_grep, {})
--- 			vim.keymap.set("n", "<C-i>", builtin.planets, {})
--- 		end,
--- 	},
--- 	{
--- 		"nvim-telescope/telescope-ui-select.nvim",
--- 		config = function()
--- 			require("telescope").setup({
--- 				defaults = {
---           layout_strategy = 'vertical',
--- 					layout_config = {
--- 						-- horizontal = {
--- 						-- 	preview_cutoff = 0,
--- 						-- },
---             vertical = { width = 0.5 },
--- 					},
--- 				},
--- 				extensions = {
--- 					["ui-select"] = {
--- 						require("telescope.themes").get_dropdown({}),
--- 					},
--- 				},
--- 			})
--- 			require("telescope").load_extension("ui-select")
--- 		end,
--- 	},
--- }
-
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 vim.keymap.set("n", "<C-o>", builtin.live_grep, {})
-vim.keymap.set("n", "<C-i>", builtin.planets, {})
 require("telescope").setup({
 	defaults = {
 		layout_strategy = "vertical",
--- lua/vim-settings.lua
+++ lua/vim-settings.lua
@@ -7,7 +7,7 @@ vim.cmd("set shiftwidth=2")
 vim.cmd(":au BufNewFile *.cpp 0r ~/.config/nvim/templates/skeleton.cpp")
 vim.cmd(":au BufNewFile *.html 0r ~/.config/nvim/templates/skeleton.html")
 vim.cmd(":au BufNewFile *.tex 0r ~/.config/nvim/templates/skeleton.tex")
-vim.cmd('highlight link @lsp.type.macro Macro')
+vim.cmd("highlight link @lsp.type.macro Macro")
 vim.cmd("let g:tex_flavor = 'latex'")
 
 vim.lsp.set_log_level("off")
@@ -18,26 +18,45 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufReadPost" }, {
 	end,
 })
 
---vim.g.mapleader = " "
-
 function OpenTerm()
 	vim.cmd(":term")
 	vim.fn.feedkeys("i")
 end
 
--- vim.lsp.set_log_level("debug")
-
 function RunJS()
 	local hint = vim.fn.input("execute with node %/npm start (f/s)? ")
 
 	if hint == "f" then
-		vim.cmd(":term node %")
-		vim.fn.feedkeys("i")
+		vim.cmd(':TermExec cmd="node %" size=40 direction=vertical')
 	elseif hint == "s" then
-		vim.cmd(":term npm start")
-		vim.fn.feedkeys("i")
+		vim.cmd(':TermExec cmd="npm start" size=40 direction=vertical')
 	end
 end
+
+-- function CompileRun()
+-- 	vim.cmd(":w") -- Save the current file
+--
+-- 	local filetype = vim.bo.filetype -- Get the current buffer's filetype
+--
+-- 	if filetype == "cpp" then
+-- 		vim.cmd(":term g++ % && ./a.out")
+-- 		vim.fn.feedkeys("i")
+-- 	elseif filetype == "javascript" then
+-- 		RunJS()
+-- 	elseif filetype == "html" then
+-- 		vim.cmd(":term open %")
+-- 		vim.fn.feedkeys("i")
+-- 	elseif filetype == "verilog" then
+-- 		vim.cmd(":term iverilog % && ./a.out")
+-- 		vim.fn.feedkeys("i")
+-- 	elseif filetype == "c" then
+-- 		vim.cmd(":term gcc % && ./a.out")
+-- 		vim.fn.feedkeys("i")
+-- 	elseif filetype == "python" then
+-- 		vim.cmd(":term python3 %")
+-- 		vim.fn.feedkeys("i")
+-- 	end
+-- end
 
 function CompileRun()
 	vim.cmd(":w") -- Save the current file
@@ -45,22 +64,15 @@ function CompileRun()
 	local filetype = vim.bo.filetype -- Get the current buffer's filetype
 
 	if filetype == "cpp" then
-		vim.cmd(":term g++ % && ./a.out")
-		vim.fn.feedkeys("i")
+		vim.cmd(':TermExec cmd="g++ % && ./a.out" size=40 direction=vertical')
 	elseif filetype == "javascript" then
 		RunJS()
 	elseif filetype == "html" then
-		vim.cmd(":term open %")
-		vim.fn.feedkeys("i")
-	elseif filetype == "verilog" then
-		vim.cmd(":term iverilog % && ./a.out")
-		vim.fn.feedkeys("i")
+		vim.cmd(':TermExec cmd="open %" size=40 direction=vertical')
 	elseif filetype == "c" then
-		vim.cmd(":term gcc % && ./a.out")
-		vim.fn.feedkeys("i")
+		vim.cmd(':TermExec cmd="gcc % && ./a.out" size=40 direction=vertical')
 	elseif filetype == "python" then
-		vim.cmd(":term python3 %")
-		vim.fn.feedkeys("i")
+    vim.cmd(':TermExec cmd="python3 %" size=40 direction=vertical')
 	end
 end
 
@@ -68,5 +80,3 @@ vim.api.nvim_set_keymap("n", "<C-x>", [[:lua CompileRun()<CR>]], { noremap = tru
 vim.api.nvim_set_keymap("n", "C", 'gg"*yG', { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "E", ":Ex<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "T", ":ToggleTerm size=40 direction=vertical<CR>", { noremap = true, silent = true })
-
---vim.api.nvim_set_keymap("n", "<c-c>", "gg y G", { noremap = true, silent = true })
