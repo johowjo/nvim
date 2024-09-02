113667b245deea6fe3de35dc26eaa0c5fd9d82d6
johowjo <johowjo@gmail.com>
2024-09-02 15:42:07 +0800

    add lazygit

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -115,4 +115,20 @@ require("lazy").setup({
   {
     "ThePrimeagen/harpoon",
   },
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
 })
