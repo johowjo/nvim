84f630ea0af6b21c009e4e2b2d9f72e4898187c0
johowjo <johowjo@gmail.com>
2024-10-07 20:46:04 +0800

    add markview.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -132,4 +132,14 @@ require("lazy").setup({
       },
     },
   },
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false, -- Recommended
+    -- ft = "markdown" -- If you decide to lazy-load anyway
+
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+  },
 })
---
+++ lua/plugins_config/markview.lua
@@ -0,0 +1,5 @@
+local presets = require("markview.presets");
+require("markview").setup({
+  checkboxes = presets.checkboxes.nerd,
+  headings = presets.headings.glow,
+})
