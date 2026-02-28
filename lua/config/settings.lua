73de86986452b877a521cda16d97a4bbf0aee5f7
johowjo <johowjo@gmail.com>
2026-02-28 23:33:12 +0800

    tweak settings

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,15 +1,18 @@
 --editor settings
-vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.rnu = true
-vim.opt.number = true
-vim.opt.backspace = { "start", "eol", "indent" }
-vim.opt.splitright = true
-vim.opt.cul = true
-vim.opt.gcr = "n-v-c-sm-i-t:block,ci-ve:ver25,r-cr-o:hor20"
-vim.opt.undofile = true
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.rnu = true
+vim.o.number = true
+vim.o.backspace = "start,eol,indent"
+vim.o.splitright = true
+vim.o.cul = true
+vim.o.gcr = "n-v-c-sm-i-t:block,ci-ve:ver25,r-cr-o:hor20"
+vim.o.undofile = true
+vim.o.path = vim.o.path .. "**"
+vim.o.wildmenu = true
 --globals
 vim.g.tex_flavor = "latex"
 vim.g.mapleader = " "
+vim.g.netrw_liststyle = 3
