a2fca5e3f2c9bfc03d45f51af0da79c055f46c21
johowjo <johowjo@gmail.com>
2026-03-06 16:04:17 +0800

    cleanup

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -1,27 +1,3 @@
--- return {
---   "rebelot/kanagawa.nvim",
---   name = "kanagawa",
---   priority = 1000,
---   lazy = false,
---   opts = {
---     undercurl = false,
---     overrides = function(colors)
---       return {
---         ErrorMsg = { fg = 12925779 },
---         DiagnosticError = { fg = 12925779 },
---         DiagnosticFloatingError = { fg = 12925779 },
---         DiagnosticSignError = { fg = 12925779 },
---         SpellBad = { sp = 12925779 },
---       }
---     end,
---   },
---   config = function(_, opts)
---     local kanagawa = require("kanagawa")
---     kanagawa.setup(opts)
---     kanagawa.load("dragon")
---   end,
--- }
-
 vim.pack.add({
   { src = "https://github.com/rebelot/kanagawa.nvim" },
 })
